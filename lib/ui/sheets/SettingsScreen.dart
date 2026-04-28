85781d4d4b72a5088adad60d8800d0d62969595f
Steve Brown <stevebrown0804@gmail.com>
2026-04-28 16:06:47 -0600

    updated a deprecated call

--- lib/ui/sheets/SettingsScreen.dart
+++ lib/ui/sheets/SettingsScreen.dart
@@ -547,7 +547,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
-      onPopInvoked: (didPop) async {
+      onPopInvokedWithResult: (didPop, result) async {
         if (didPop) return;
         await _attemptLeaveSettings();
       },
